312f20eed7a4b2c85f11bda3fad7b88f0f21eeee
gabrielesanti11 <gabrielesanti@live.com>
2026-02-11 18:44:42 +0100

    sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,27 @@
-self.addEventListener("install", () => self.skipWaiting());
-self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
+const CACHE = "cpaapmal-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./pagheicon-192.png",
+  "./pagheicon-512.png",
+  "./level-up-08-402152.mp3"
+];
+
+self.addEventListener("install", (e) => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE ? caches.delete(k) : null)))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => r || fetch(e.request))
+  );
+});
